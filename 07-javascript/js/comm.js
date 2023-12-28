829e46389ea0e85c2f012884087c5b1e2b39954a
study-MY <null.for.ez@gmail.com>
2023-12-28 14:32:39 +0900

    js study

--- 07-javascript/js/comm.js
+++ 07-javascript/js/comm.js
@@ -5,6 +5,7 @@
 
 // var, let, const
 // var : ES6 이전에 사용하던 변수. (현재는 잘 사용하지 않음.)
+// var는 재할당이 가능. 그렇게 되면 가장 마지막에 적힌 코드가 적용되기 때문에 오류가 많이 발생. 예를 들면 스와이퍼 1을 만들고 스와이퍼 2를 만들었을 때 스와이퍼 2가 1에 적용되어 오류가 생긴다던가.
 
 // 변수 선언
 let a = 10;
@@ -105,3 +106,121 @@ console.log(user.name);
 console.log(user.age);
 console.log(user.cute);
 console.log(`${user.name}의 나이는 ${user.age}살 입니다. ${user.name}은 ${user.cute ? "귀여워요." : "귀엽지않아요."}`)
+
+// 배열(Array)
+// 여러 데이터를 순서대로 저장하기 위해 사용
+
+let fruits = ['바나나', 'Apple', '체리'];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// 예약어
+// this 자신, if, break
+
+// 함수(Function)
+// 특정 기능을 수행하는 코드의 집합
+
+// 함수 선언
+function sayHello() {
+    // 함수 내용
+    console.log('Hello!');
+}
+
+// 함수 호출
+sayHello();
+sayHello();
+
+// 함수 내보내기
+// return 내보내기
+
+function returnFunc() {
+    return 123;
+};
+let f = returnFunc();
+console.log(f);
+
+// 함수의 재사용
+// 예: 국어, 수학 점수를 전달하면 총점을 계산해주는 함수
+
+// 1 함수 선언
+// function get();으로 일단 값을 받을 건데 이건 나중에.
+function sum(a, b) {
+    // a, b는 매개변수(Parameters)
+    return a + b;
+}
+// 2 함수 호출
+sum(88, 76);
+console.log(sum(88, 76));
+console.log(sum(70, 80));
+console.log(sum(60, 60));
+
+let ab = sum(20, 30);
+console.log(ab);
+
+// 기명함수 (Named Function)
+function hello() {
+    console.log(`Hello! 나는 ${myName}이야!`);
+};
+hello();
+
+// 익명함수 (Anonymous Function)
+let hello2 = function() {
+    console.log(`Nice to meet you!`)
+};
+hello2();
+
+// 화살표 함수 (Arrow Function)
+let hello3 = () => {
+    console.log('hello3')
+};
+// ()매개변수
+// {}실행
+
+hello3();
+
+// 조건문 (if, else)
+// 특정 조건에 따라 다른 코드를 실행할 때 사용
+// 참과 거짓일 때의 조건을 모두 집어넣고 싶다면 if else
+
+// if (조건) {참일 경우 실행문} else {거짓일 경우 실행문}
+let isShow = true;
+let checked = false;
+// false면 실행되지 않음.
+
+if(isShow) {console.log('Show!')};
+if(checked) {console.log('Checked!');}
+
+if(true) {
+    console.log('true');
+} else {
+    console.log('false')
+}
+
+// DOM API
+// Document Object Model, Application Programming Interface
+
+// HTML 요소 (Element) 검색
+
+const boxEl = document.querySelector('.box')
+// class 값이 box인 텍스트 검색에 가까움.
+// 데이터 검색이 아님.
+
+// boxEl를 클릭했을 때, 할 일을 함수로 정의
+boxEl.addEventListener('click',function () {
+    console.log('Click');
+    // boxEl 요소에 active 클래스 추가
+    boxEl.classList.add('active')
+});
+// 이벤트Event가 발생add하는 지 듣고 있도록Listener
+// ("click",function () {}) = (Event : 상황,  Handler : 무엇을 실행할지 )
+
+const tabEls = document.querySelectorAll('.tab');
+console.log(tabEls);
+
+tabEls.forEach(function (tabEl, index) {
+    tabEl.classList.add(`tab-${index + 1}`);
+    console.log(tabEl, index);
+});
