c421b562b05453d6976eb46f2c306efe2af92ab8
study-MY <null.for.ez@gmail.com>
2024-01-23 17:20:23 +0900

    node_js_study

---
+++ 09-node_js/main.js
@@ -0,0 +1,24 @@
+import Swiper from "swiper";
+import "swiper/swiper.css";
+// const swiper = new Swiper("엘리먼트 이름, {옵션 은 객체 형태로로 입력되는. key : value}");
+const swiper = new Swiper("엘리먼트 이름", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
