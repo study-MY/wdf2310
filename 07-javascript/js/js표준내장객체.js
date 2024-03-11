a4adfbb49c7d45d14ec7c6a508d1b7066af91be4
study-MY <null.for.ez@gmail.com>
2024-03-11 13:13:02 +0900

    표준내장객체 실습

---
+++ 07-javascript/js/js표준내장객체.js
@@ -0,0 +1,13 @@
+const now = new Date();
+
+console.log("날짜");
+console.log(new Date());
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth() + 1);
+console.log(new Date().getDay());
+
+console.log("시간");
+console.log(now.getHours());
+console.log(new Date().getMinutes());
+
+console.log(new Date().getTime());
